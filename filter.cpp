0cd69e0f58494ac4ed2c5a00fdf0bc194260eae2
hyukjin17 <chung.hy@northeastern.edu>
2026-01-14 16:45:43 -0500

    feat: implement new optimized blur filter (only first horizontal pass)

--- filter.cpp
+++ filter.cpp
@@ -21,7 +21,8 @@ int greyscale(cv::Mat &src, cv::Mat &dst)
             // uchar green = 255 - ptr[j][1];
             uchar blue = 255 - ptr[j][0];
             uchar grey = (uchar)(0.114 * blue + 0.587 * ptr[j][1] + 0.299 * ptr[j][2]);
-            for (int k = 0; k < 3; k++) {
+            for (int k = 0; k < 3; k++)
+            {
                 ptr[j][k] = grey;
             }
         }
@@ -49,9 +50,12 @@ int sepia(cv::Mat &src, cv::Mat &dst)
             int newRed = 0.393 * red + 0.769 * green + 0.189 * blue;
 
             // clip any values greater than 255
-            if (newBlue > 255) newBlue = 255;
-            if (newGreen > 255) newGreen = 255;
-            if (newRed > 255) newRed = 255;
+            if (newBlue > 255)
+                newBlue = 255;
+            if (newGreen > 255)
+                newGreen = 255;
+            if (newRed > 255)
+                newRed = 255;
 
             // assign new values to the pixels
             ptr[j][0] = (uchar)newBlue;
@@ -62,7 +66,7 @@ int sepia(cv::Mat &src, cv::Mat &dst)
     return (0);
 }
 
-// 5x5 blur filter using integer approximation of Gaussian
+// 5x5 blur filter using integer approximation of Gaussian (smoothing)
 int blur5x5_1(cv::Mat &src, cv::Mat &dst)
 {
     src.copyTo(dst); // makes a copy of the image
@@ -75,17 +79,22 @@ int blur5x5_1(cv::Mat &src, cv::Mat &dst)
         {1, 2, 4, 2, 1}};
 
     // loop over all pixels except the outer two rows and columns
-    for (int i = 2; i < dst.rows-2; i++) {
-        for (int j = 2; j < dst.cols-2; j++) {
-            for (int k = 0; k < 3; k++) { // loop over RGB color channels
+    for (int i = 2; i < dst.rows - 2; i++)
+    {
+        for (int j = 2; j < dst.cols - 2; j++)
+        {
+            for (int k = 0; k < 3; k++) // loop over RGB color channels
+            {
                 int sum = 0; // sum of all neighboring pixel values multiplied by the blur matrix
-                for (int y = -2; y < 3; y++) {
-                    for (int x = -2; x < 3; x++) {
+                for (int y = -2; y < 3; y++)
+                {
+                    for (int x = -2; x < 3; x++)
+                    {
                         // sum the values from the original src image
-                        sum += src.at<cv::Vec3b>(i + y, j + x)[k] * blur[y+2][x+2];
+                        sum += src.at<cv::Vec3b>(i + y, j + x)[k] * blur[y + 2][x + 2];
                     }
                 }
-                sum = sum / 100; // divide by the sum of values in the blur matrix to normalize
+                sum = sum / 100;                         // divide by the sum of values in the blur matrix to normalize
                 dst.at<cv::Vec3b>(i, j)[k] = (uchar)sum; // update the dst image
             }
         }
@@ -93,3 +102,42 @@ int blur5x5_1(cv::Mat &src, cv::Mat &dst)
     return (0);
 }
 
+// faster implementation of a 5x5 blur filter using separable horizontal and vertical filters
+int blur5x5_2(cv::Mat &src, cv::Mat &dst)
+{
+    cv::Mat temp;
+    src.copyTo(temp); // makes an intermediate temp copy of the image
+    src.copyTo(dst);  // makes a copy of the image for the final blur
+
+    int blur[5] = {1, 2, 4, 2, 1};
+
+    // first pass (horizontal blur)
+    // loop over every row
+    for (int i = 0; i < dst.rows; i++)
+    {
+        cv::Vec3b *srcPtr = src.ptr<cv::Vec3b>(i);   // gets the row pointer from the src image
+        cv::Vec3b *tempPtr = temp.ptr<cv::Vec3b>(i); // gets the row pointer from the temp image
+        cv::Vec3b *tempPtr = dst.ptr<cv::Vec3b>(i); // gets the row pointer from the dst image
+        // loop over the columns (except the first and last two)
+        for (int j = 2; j < dst.cols - 2; j++)
+        {
+            // loop over RGB color channels
+            for (int k = 0; k < 3; k++)
+            {
+                int sum = 0; // sum of the horizontally neighboring pixel values
+                for (int x = -2; x < 3; x++)
+                {
+                    // sum the values from the original src image
+                    sum += srcPtr[j + x][k] * blur[x + 2];
+                }
+                sum = sum / 10;             // divide by the sum of values in the blur vector to normalize
+                tempPtr[j][k] = (uchar)sum; // update the temp image
+                dstPtr[j][k] = (uchar)sum;  // for testing only
+            }
+        }
+    }
+
+
+
+    return (0);
+}
